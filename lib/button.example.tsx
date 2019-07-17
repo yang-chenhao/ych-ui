90f73cab833c98b35900b06856fa122b36e9b543
yangchenhao <494753013@qq.com>
2019-07-17 22:38:55 +0800

    add examples

---
+++ lib/button.example.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ButtonExample: React.FunctionComponent = () => {
+    return (
+        <div>hiiiiii</div>
+    )
+}
+
+export default ButtonExample
---
+++ lib/icon/icon.example.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Icon from './icon'
+
+const IconExample: React.FunctionComponent = () => {
+    return (
+        <div>
+            <Icon name='qq'></Icon>
+            <Icon name='alipay'></Icon>
+            <Icon name='wechat'></Icon>
+        </div>
+    )
+}
+
+export default IconExample
