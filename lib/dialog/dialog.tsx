36d594d0e750a428e2f8f02f7ef70de9d4c6cb95
yangchenhao <494753013@qq.com>
2019-07-23 22:26:42 +0800

    将dialog传送至body

--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -2,6 +2,7 @@ import React, { Fragment, ReactElement } from 'react'
 import './dialog.scss'
 import { Icon } from '../index'
 import { scopedClassMaker } from '../classes'
+import ReactDOM from 'react-dom';
 
 interface Props {
     visible: boolean,
@@ -20,29 +21,30 @@ const Dialog: React.FunctionComponent<Props> = (props) => {
             props.onClose(e)
         }
     }
+    const x = props.visible ?
+    <Fragment>
+        <div className={sc('mask')} onClick={onClickMask}>
+        </div>
+        <div className={sc()}>
+            <div className={sc('close')}>
+                <Icon name='close' onClick={onClickClose} />
+            </div>
+            <header className={sc('header')}>
+                提示
+            </header>
+            <main className={sc('main')}>
+                {props.children}
+            </main>
+            <footer className={sc('footer')}>
+                {props.buttons.map((button, index) =>
+                    React.cloneElement(button, { key: index })
+                )}
+            </footer>
+        </div>
+    </Fragment> :
+    null
     return (
-        props.visible ?
-            <Fragment>
-                <div className={sc('mask')} onClick={onClickMask}>
-                </div>
-                <div className={sc()}>
-                    <div className={sc('close')}>
-                        <Icon name='close' onClick={onClickClose} />
-                    </div>
-                    <header className={sc('header')}>
-                        提示
-                    </header>
-                    <main className={sc('main')}>
-                        {props.children}
-                    </main>
-                    <footer className={sc('footer')}>
-                        {props.buttons.map((button, index) =>
-                            React.cloneElement(button, { key: index })
-                        )}
-                    </footer>
-                </div>
-            </Fragment> :
-            null
+        ReactDOM.createPortal(x, document.body)
     )
 }
 
